b62d1ef8054d0a95f4cbdcb6ebeafbe1da3c9806
Hemanth Guvvala <hemanthkumarguvala@gmail.com>
2026-07-01 18:54:53 +0530

    Initial Spring Boot project setup

---
+++ src/main/java/com/assignment/graph/TransactionGraphNodeExplorerApplication.java
@@ -0,0 +1,15 @@
+package com.assignment.graph;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Entry point for the Transaction Graph Node Explorer service.
+ */
+@SpringBootApplication
+public class TransactionGraphNodeExplorerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TransactionGraphNodeExplorerApplication.class, args);
+    }
+}
